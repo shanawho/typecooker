b72a0662002a4e654628b464b4c7437be7fbe56a
Shana Hu <shanawho@gmail.com>
2020-02-03 19:47:13 -0800

    Initial Commit

---
+++ typecooker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  typecooker
+//
+//  Created by Shana Hu on 2/3/20.
+//  Copyright © 2020 Shana Hu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
